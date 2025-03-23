26a56e5521387bda20b8874a94ccf3647282b321
Holger Graef <holger.graef@gmail.com>
2025-03-23 22:16:06 +0100

    read wifi/device config from flash, add access through USB MSC

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <Arduino_GFX_Library.h>
 #include <CST816S.h>
+#include <FFat.h>
 #include <lvgl.h>
 #include <tuyacpp/scanner.hpp>
+#include <USB.h>
+#include <USBMSC.h>
 #include <WiFi.h>
 
 #include "pin_config.h"
-#include "wifi_config.h"
-#include "devices.json.h"
+
+#define BLOCK_SIZE 4096
+
+USBCDC cdc;
+USBMSC msc;
+EspClass esp;
+const esp_partition_t* partition;
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(LCD_DC, LCD_CS, LCD_SCK, LCD_MOSI);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, LCD_RST, 0, true, LCD_WIDTH, LCD_HEIGHT, 0, 20, 0, 0);
@@ -22,6 +30,13 @@ std::unique_ptr<tuya::Scanner> s;
 
 static lv_obj_t *battery_label;
 static lv_obj_t *wifi_label;
+static bool fs_mounted = false;
+static bool devices_loaded = false;
+static bool wifi_config_loaded = false;
+static ordered_json devices_data;
+static ordered_json wifi_config;
+static std::string wifi_ssid;
+static std::string wifi_password;
 
 static uint32_t loop_counter = 0;
 
@@ -63,7 +78,40 @@ void example_increase_lvgl_tick(void *arg) {
   lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
 }
 
+static int write_to_cdc(void *cookie, const char *data, int size) {
+  cdc.write(data, size);
+  return size;
+}
+
 void setup() {
+  cdc.begin();
+  cdc.setDebugOutput(true);
+
+  fclose(stdout);
+  stdout = funopen(nullptr, nullptr, &write_to_cdc, nullptr, nullptr);
+  fclose(stderr);
+  stderr = funopen(nullptr, nullptr, &write_to_cdc, nullptr, nullptr);
+
+  partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_FAT, NULL);
+  fs_mounted = FFat.begin();
+
+  msc.onRead([](uint32_t lba, uint32_t offset, void *buffer, uint32_t bufsize) {
+    std::cout << "USB MSC read: LBA " << lba << std::endl;
+    esp.partitionRead(partition, offset + lba * BLOCK_SIZE, (uint32_t *) buffer, bufsize);
+    return (int) bufsize;
+  });
+  msc.onWrite([](uint32_t lba, uint32_t offset, uint8_t *buffer, uint32_t bufsize) {
+    std::cout << "USB MSC write: LBA " << lba << std::endl;
+    esp.partitionEraseRange(partition, offset + lba * BLOCK_SIZE, bufsize);
+    esp.partitionWrite(partition, offset + lba * BLOCK_SIZE, (uint32_t *) buffer, bufsize);
+    return (int) bufsize;
+  });
+  msc.mediaPresent(true);
+  msc.begin(partition->size / BLOCK_SIZE, BLOCK_SIZE);
+
+  USB.serialNumber("");
+  USB.begin();
+
   pinMode(BUZZER, OUTPUT);
   pinMode(SYS_EN, OUTPUT);
   pinMode(VOLTAGE_DIVIDER, INPUT);
@@ -103,8 +151,31 @@ void setup() {
   indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
 
-  std::string data((char *) devices_json, devices_json_len);
-  s = std::make_unique<tuya::Scanner>(l, ordered_json::parse(data));
+  if (FFat.exists("/devices.json")) {
+    File f = FFat.open("/devices.json");
+    try {
+      devices_data = ordered_json::parse(f.readString());
+      devices_loaded = true;
+    } catch (const std::exception& e) {
+      std::cout << "Error parsing devices.json: " << e.what() << std::endl;
+    }
+    f.close();
+  }
+
+  if (FFat.exists("/wifi.json")) {
+    File f = FFat.open("/wifi.json");
+    try {
+      wifi_config = ordered_json::parse(f.readString());
+      wifi_ssid = wifi_config["ssid"].get<std::string>();
+      wifi_password = wifi_config["password"].get<std::string>();
+      wifi_config_loaded = true;
+    } catch (const std::exception& e) {
+      std::cout << "Error parsing wifi.json: " << e.what() << std::endl;
+    }
+    f.close();
+  }
+
+  s = std::make_unique<tuya::Scanner>(l, devices_data);
 
   lv_obj_t * cont_col = lv_obj_create(lv_scr_act());
   lv_obj_align(cont_col, LV_ALIGN_CENTER, 0, 0);
@@ -131,6 +202,21 @@ void setup() {
   wifi_label = lv_label_create(cont_col);
   lv_label_set_text(wifi_label, "Initializing...");
 
+  if (!fs_mounted || !devices_loaded || !wifi_config_loaded) {
+    lv_obj_t* warning_label = lv_label_create(cont_col);
+    lv_label_set_text(warning_label, "Warning: Storage not\nformatted or data missing");
+    lv_obj_t* format_button = lv_btn_create(cont_col);
+    lv_obj_t* format_label = lv_label_create(format_button);
+    lv_label_set_text(format_label, "Format storage");
+    lv_obj_add_event_cb(format_button, [](lv_event_t *event) {
+      if (event->code == LV_EVENT_CLICKED) {
+        FFat.end();
+        FFat.format();
+        ESP.restart();
+      }
+    }, LV_EVENT_CLICKED, NULL);
+  }
+
   const esp_timer_create_args_t lvgl_tick_timer_args = {
     .callback = &example_increase_lvgl_tick,
     .name = "lvgl_tick"
@@ -140,9 +226,10 @@ void setup() {
   esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer);
   esp_timer_start_periodic(lvgl_tick_timer, EXAMPLE_LVGL_TICK_PERIOD_MS * 1000);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  std::cout << "Connecting to the WiFi network" << std::endl;
+  if (wifi_config_loaded) {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wifi_ssid.c_str(), wifi_password.c_str());
+  }
 }
 
 void loop() {
